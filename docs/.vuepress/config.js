eb69604d5918af117dcd114add4de582053e0f72
qiuyang <1736315428@qq.com>
2019-11-05 14:45:35 +0800

    add blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,13 +27,6 @@ module.exports = {
   },
   head: [
     ['link', { rel: 'icon', href: '/Q.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/Q.png' }],
-    ['link', { rel: 'mask-icon', href: '/Q.png', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/Q.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['link', { rel: 'manifest', href: '/manifest.json' }]
   ],
 }
